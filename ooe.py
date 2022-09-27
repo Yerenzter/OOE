c4c3e62d136848e218a8e8429944e84d4c49e72e
Yerenzter <73117946+Yerenzter@users.noreply.github.com>
2022-09-27 13:52:35 +0800

    --ooe

---
+++ ooe.py
@@ -0,0 +1,21 @@
+# ODD or EVEN Python3 Library
+# Created by Yerenzter
+# Under the Mozilla 2.0 Public License
+
+def isOdd(number):
+    if number % 2 != 0:
+        return str(number) + " is an odd number"
+    else:
+        return str(number) + " is not an odd number"
+
+def isEven(number):
+    if number % 2 == 0:
+        return str(number) + " is an even number"
+    else:
+        return str(number) + " is not an even number"
+
+def OOE(number):
+    if number % 2 == 0:
+        return str(number) + " is an even number"
+    else:
+        return str(number) + " is an odd number"
